2b931b29d2c13029093938af94737ff123eafc67
Boolshyt <chakeriototo@gmail.com>
2021-01-13 00:49:36 +0700

    34:40

---
+++ src/components/Form.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+
+
+const Form = ({ inputText, setInputText, todos, setTodos }) => {
+    const inputTextHandler = (e) => {
+        setInputText(e.target.value);
+    }
+
+    const submitTodoHandler = (e) => {
+        e.preventDefault();
+        setTodos([...todos, { text: inputText, completed: false }])
+        setInputText("");
+    }
+
+    return (
+        <form>
+            <input onChange={inputTextHandler} type="text" className="todo-input" value={inputText} />
+            <button onClick={submitTodoHandler} type="submit" className="todo-button" >
+                <i className="fas fa-plus-square" ></i>
+            </button>
+            <div className="select">
+                <select name="todos" className="filter-todo">
+                    <option value="all">All</option>
+                    <option value="completed">Completed</option>
+                    <option value="uncompleted">Uncompleted</option>
+                </select>
+            </div>
+
+        </form>
+    );
+
+}
+
+
+
+export default Form;
---
+++ src/components/TodoList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Todolist = () => {
+    return (
+        <div className="todo-container">
+            <ul className="todo-list">
+
+            </ul>
+        </div>
+    );
+}
+
+
+export default Todolist;
